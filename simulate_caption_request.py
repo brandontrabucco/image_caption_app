5d7cdc95520e9844f218fea05fe8256f6479b298
Brandon Trabucco <brandontrabucco@gmail.com>
2018-06-12 10:46:27 -0400

    added terminal args

--- simulate_caption_request.py
+++ simulate_caption_request.py
@@ -4,7 +4,7 @@ import json
 
 class HandleRequest(object):
     
-    def __init__(self, image_uri):
+    def __init__(self, image_uri, server_url):
         with tf.gfile.GFile(
                 image_uri, "rb") as f:
             self.r = rq.post(
@@ -24,8 +24,12 @@ if __name__ == "__main__":
         "--image_uri",
         type=str,
         default="/home/ubuntu/im2txt/im2txt/data/coco/raw-data/val2014/COCO_val2014_000000224477.jpg")
+    parser.add_argument(
+        "--server_url",
+        type=str,
+        default="http://ec2-18-216-95-59.us-east-2.compute.amazonaws.com")
     args = parser.parse_args()
-    r = HandleRequest(args.image_uri)
+    r = HandleRequest(args.image_uri, args.server_url)
     print("Image Caption for %s:"%args.image_uri)
     for i, caption in enumerate(r.caption()):
         print(" %d) %s (p=%f)" % (i, caption[0], caption[1]))
