90cb403b10373ad0cc64f75b6b7f7a18459abce1
Ubuntu <ubuntu@ip-172-31-40-239.us-east-2.compute.internal>
2018-07-02 18:56:00 +0000

    updated im2txt api

--- image_captioning_app.py
+++ image_captioning_app.py
@@ -1,5 +1,5 @@
 from flask import Flask, abort, request
-from im2txt import run_once
+from im2txt import run_caption
 import json
 
 app = Flask(__name__)
@@ -9,7 +9,7 @@ def hello_world():
   if not request.data:
     return json.dumps({"image_caption": "nothing to see here."})
 
-  model_output = run_once(request.data)
+  model_output = run_caption(request.data)
   return json.dumps({"image_caption": model_output})
 
 if __name__ == '__main__':
